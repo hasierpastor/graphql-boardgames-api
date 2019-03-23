0602a9dd1b1180cb38140ab9bfa29e9d67330c81
Hasier Pastor <hasier.pastor@gmail.com>
2019-03-23 11:55:30 -0700

    created mongoose boardgame schema

---
+++ models/boardgame.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const boardgameSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  designer: {
+    type: String,
+    required: true
+  },
+  publisher: String,
+  rating: Number,
+  weight: Number,
+  category: String,
+  description: String
+});
+
+/* 
+create our model from the schema to perform CRUD actions on our documents 
+ (which are objects created from the model constructor)
+*/
+const BoardGame = mongoose.model('BoardGame', boardgameSchema);
+module.exports = BoardGame;
